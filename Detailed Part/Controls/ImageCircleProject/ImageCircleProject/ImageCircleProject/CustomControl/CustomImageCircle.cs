700f9a2a9e1bc70b609f5d59a51877dde1a7bfe9
Maxime GUITTET <maxime.guittet@epitech.eu>
2016-07-17 00:09:04 +0200

    ImageCircle -- (Android, UWP) OK -- (iOS) KO

---
+++ Detailed Part/Controls/ImageCircleProject/ImageCircleProject/ImageCircleProject/CustomControl/CustomImageCircle.cs
@@ -0,0 +1,9 @@
+﻿using Xamarin.Forms;
+
+namespace ImageCircleProject.CustomControl
+{
+    /// <summary>
+    /// CustomImageCircle only inherit from Image. It's into the Renderer that everything is made to make the image as circle.
+    /// </summary>
+    public class CustomImageCircle : Image { }
+}
---
+++ Detailed Part/Controls/ImageCircleProject/ImageCircleProject/ImageCircleProject/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace ImageCircleProject
+{
+    public partial class MainPage : ContentPage, INotifyPropertyChanged
+    {
+        private ImageSource imagePath;
+        public ImageSource ImagePath
+        {
+            get { return this.imagePath; }
+            set
+            {
+                this.imagePath = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainPage()
+        {
+            base.BindingContext = this;
+            ImagePath = ImageSource.FromResource("ImageCircleProject.Image.FullBlack.png");
+            InitializeComponent();
+        }
+    }
+}
