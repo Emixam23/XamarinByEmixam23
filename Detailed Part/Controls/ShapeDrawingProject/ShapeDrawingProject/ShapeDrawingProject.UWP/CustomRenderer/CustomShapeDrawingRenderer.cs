ea62d9a5960e78e8cc25c0351abd5a2ae8e418b0
Maxime GUITTET <maxime.guittet@epitech.eu>
2016-09-13 10:33:42 +0200

    general update + add of ShapeDrawingProject (Only ok for UWP)

---
+++ Detailed Part/Controls/ShapeDrawingProject/ShapeDrawingProject/ShapeDrawingProject.UWP/CustomRenderer/CustomShapeDrawingRenderer.cs
@@ -0,0 +1,74 @@
+﻿using ShapeDrawingProject.CustomControl;
+using ShapeDrawingProject.UWP.CustomRenderer;
+using System;
+using System.ComponentModel;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Shapes;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.UWP;
+using static ShapeDrawingProject.CustomControl.CustomShapeDrawing;
+
+[assembly: ExportRenderer(typeof(CustomShapeDrawing), typeof(CustomShapeDrawingRenderer))]
+namespace ShapeDrawingProject.UWP.CustomRenderer
+{
+    public class CustomShapeDrawingRenderer : ViewRenderer<CustomShapeDrawing, Polygon>
+    {
+        CustomShapeDrawing customShapeDrawing;
+
+        public CustomShapeDrawingRenderer()
+        {
+            SizeChanged += CustomShapeDrawingRenderer_SizeChanged;
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<CustomShapeDrawing> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.NewElement != null)
+            {
+                customShapeDrawing = e.NewElement as CustomShapeDrawing;
+                DrawCustomShapeDrawing();
+            }
+        }
+
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+
+            if (e.PropertyName == CustomShapeDrawing.BorderColorProperty.PropertyName)
+                DrawCustomShapeDrawing();
+        }
+
+        private void CustomShapeDrawingRenderer_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            DrawCustomShapeDrawing();
+        }
+
+        private void DrawCustomShapeDrawing()
+        {
+            if (customShapeDrawing != null)
+            {
+                var poly = new Polygon();
+
+                poly.DataContext = Element;
+
+                poly.Fill = (SolidColorBrush)new ColorConverter().Convert(customShapeDrawing.ShapeColor, null, null, null);
+                poly.Stroke = (SolidColorBrush)new ColorConverter().Convert(customShapeDrawing.BorderColor, null, null, null);
+
+                poly.Points = new PointCollection();
+
+                Xamarin.Forms.Rectangle rect = new Xamarin.Forms.Rectangle(0, 0, customShapeDrawing.Width, customShapeDrawing.Height);
+
+                rect.Size = new Size(customShapeDrawing.Width, customShapeDrawing.Height);
+                foreach (XYCoordinate xy in customShapeDrawing.PointsShapeCoordinateList)
+                {
+                    poly.Points.Add(new Windows.Foundation.Point(rect.Width * xy.X, rect.Height * xy.Y));
+                }
+                poly.FillRule = FillRule.EvenOdd;
+
+                SetNativeControl(poly);
+            }
+        }
+    }
+}
---
+++ Detailed Part/Controls/ShapeDrawingProject/ShapeDrawingProject/ShapeDrawingProject/CustomControl/CustomShapeDrawing.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Xamarin.Forms;
+
+namespace ShapeDrawingProject.CustomControl
+{
+    public class CustomShapeDrawing : View
+    {
+        /// <summary>
+        /// The BorderColor property.
+        /// </summary>
+        public static readonly BindableProperty BorderColorProperty =
+            BindableProperty.Create(nameof(BorderColor), typeof(Color), typeof(CustomShapeDrawing), Color.Black);
+
+        /// <summary>
+        /// Assessor for the BorderColor property.
+        /// </summary>
+        public Color BorderColor
+        {
+            get { return (Color)GetValue(BorderColorProperty); }
+            set { SetValue(BorderColorProperty, value); }
+        }
+
+        /// <summary>
+        /// The BorderColor property.
+        /// </summary>
+        public static readonly BindableProperty ShapeColorProperty =
+            BindableProperty.Create(nameof(ShapeColor), typeof(Color), typeof(CustomShapeDrawing), Color.White);
+
+        /// <summary>
+        /// Assessor for the BorderColor property.
+        /// </summary>
+        public Color ShapeColor
+        {
+            get { return (Color)GetValue(ShapeColorProperty); }
+            set { SetValue(ShapeColorProperty, value); }
+        }
+
+        /// <summary>
+        /// The PointsShapeCoordinate property.
+        /// </summary>
+        public static readonly BindableProperty PointsShapeCoordinateProperty =
+            BindableProperty.Create(nameof(PointsShapeCoordinate), typeof(string), typeof(CustomShapeDrawing), null);
+
+        /// <summary>
+        /// Assessor for the PointsShapeCoordinate property.
+        /// </summary>
+        public string PointsShapeCoordinate
+        {
+            get { return (string)GetValue(PointsShapeCoordinateProperty); }
+            set { SetValue(PointsShapeCoordinateProperty, value); }
+        }
+
+        /// <summary>
+        /// Return a list of X/Y coordinates from the PointsShapeCoordinate string property.
+        /// </summary>
+        public List<XYCoordinate> PointsShapeCoordinateList
+        {
+            get
+            {
+                return (PointsShapeCoordinateToList(this.PointsShapeCoordinate));
+            }
+        }
+
+        /// <summary>
+        /// Return a list of X/Y coordinates from a string which contains the point as string format.
+        /// </summary>
+        /// <param name="PointsShapeCoordinate">X/Y coordinate as string format.</param>
+        /// <returns>Return a list of X/Y coordinates from the PointsShapeCoordinate string property.</returns>
+        public static List<XYCoordinate> PointsShapeCoordinateToList(string PointsShapeCoordinate)
+        {
+            List<XYCoordinate> Points = new List<XYCoordinate>();
+
+            if (PointsShapeCoordinate != null)
+            {
+                string[] pointsTab = PointsShapeCoordinate.Split(",".ToCharArray());
+                foreach (string coordinate in pointsTab)
+                {
+                    string[] pointXY = coordinate.Split("/".ToCharArray());
+                    XYCoordinate xy = new XYCoordinate();
+
+                    try
+                    {
+                        xy.X = Convert.ToDouble(pointXY[0]);
+                        xy.Y = Convert.ToDouble(pointXY[1]);
+                    }
+                    catch (FormatException e)
+                    {
+                        Debug.WriteLine("FormatException: " + e.ToString());
+                    }
+                    catch (NullReferenceException e)
+                    {
+                        Debug.WriteLine("NullReferenceException: " + e.ToString());
+                    }
+                    catch (Exception e)
+                    {
+                        Debug.WriteLine("Exception: " + e.ToString());
+                    }
+                    finally
+                    {
+                        Points.Add(xy);
+                    }
+                }
+            }
+
+            return (Points);
+        }
+
+        public class XYCoordinate
+        {
+            public double X { get; set; }
+            public double Y { get; set; }
+        }
+    }
+}
