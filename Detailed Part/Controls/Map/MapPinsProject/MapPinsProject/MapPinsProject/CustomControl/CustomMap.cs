fd9ada1c881ab1fa364097c3524c0516a0191cbd
Maxime Guittet <Maxime Guittet>
2016-09-20 15:02:29 +0200

    little update of the git

--- Detailed Part/Controls/Map/MapPinsProject/MapPinsProject/MapPinsProject/CustomControl/CustomMap.cs
+++ Detailed Part/Controls/Map/MapPinsProject/MapPinsProject/MapPinsProject/CustomControl/CustomMap.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using Xamarin.Forms;
 using Xamarin.Forms.Maps;
 
@@ -39,194 +40,69 @@ namespace MapPinsProject.CustomControl
             set { SetValue(CameraFocusParameterProperty, value); }
         }
 
-        #region Camera focus method
+        #region Camera focus definition
+        public class CameraFocusData
+        {
+            public Position Position { get; set; }
+            public Distance Distance { get; set; }
+        }
         private static void OnCustomPinsPropertyChanged(BindableObject bindable, object oldValue, object newValue)
         {
             CustomMap customMap = ((CustomMap)bindable);
 
             if (customMap.CameraFocusParameter == CameraFocusReference.OnPins)
             {
-                List<Position> PositionPins = new List<Position>();
-                bool onlyOnePointPresent;
+                List<double> latitudes = new List<double>();
+                List<double> longitudes = new List<double>();
 
                 foreach (CustomPin pin in (newValue as List<CustomPin>))
                 {
-                    PositionPins.Add(pin.Position);
+                    latitudes.Add(pin.Position.Latitude);
+                    longitudes.Add(pin.Position.Longitude);
                 }
-                Position CentralPosition = GetCentralPosition(PositionPins);
-                if (PositionPins.Count > 1)
-                {
-                    Position[] FarestPoints = GetTwoFarestPointsOfCenterPointReference(PositionPins, CentralPosition);
-                    customMap.CameraFocus = GetPositionAndZoomLevelForCameraAboutPositions(FarestPoints);
-                    onlyOnePointPresent = false;
-                }
-                else
-                {
-                    customMap.CameraFocus = new CameraFocusData() { Position = CentralPosition };
-                    onlyOnePointPresent = true;
-                }
-                customMap.MoveToRegion(MapSpan.FromCenterAndRadius(customMap.CameraFocus.Position,
-                (!onlyOnePointPresent) ? (customMap.CameraFocus.Distance) : (new Distance(5))));
+
+                double lowestLat = latitudes.Min();
+                double highestLat = latitudes.Max();
+                double lowestLong = longitudes.Min();
+                double highestLong = longitudes.Max();
+                double finalLat = (lowestLat + highestLat) / 2;
+                double finalLong = (lowestLong + highestLong) / 2;
+
+                double distance = DistanceCalculation.GeoCodeCalc.CalcDistance(lowestLat, lowestLong, highestLat, highestLong, DistanceCalculation.GeoCodeCalcMeasurement.Kilometers);
+
+                customMap.MoveToRegion(MapSpan.FromCenterAndRadius(new Position(finalLat, finalLong), Distance.FromKilometers(distance * 0.7)));
             }
         }
-        public static Position GetCentralPosition(List<Position> positions)
+        private class DistanceCalculation
         {
-            if (positions.Count == 1)
+            public static class GeoCodeCalc
             {
-                foreach (Position pos in positions)
+                public const double EarthRadiusInMiles = 3956.0;
+                public const double EarthRadiusInKilometers = 6367.0;
+
+                public static double ToRadian(double val) { return val * (Math.PI / 180); }
+                public static double DiffRadian(double val1, double val2) { return ToRadian(val2) - ToRadian(val1); }
+
+                public static double CalcDistance(double lat1, double lng1, double lat2, double lng2)
                 {
-                    return (pos);
+                    return CalcDistance(lat1, lng1, lat2, lng2, GeoCodeCalcMeasurement.Miles);
+                }
+
+                public static double CalcDistance(double lat1, double lng1, double lat2, double lng2, GeoCodeCalcMeasurement m)
+                {
+                    double radius = GeoCodeCalc.EarthRadiusInMiles;
+
+                    if (m == GeoCodeCalcMeasurement.Kilometers) { radius = GeoCodeCalc.EarthRadiusInKilometers; }
+                    return radius * 2 * Math.Asin(Math.Min(1, Math.Sqrt((Math.Pow(Math.Sin((DiffRadian(lat1, lat2)) / 2.0), 2.0) + Math.Cos(ToRadian(lat1)) * Math.Cos(ToRadian(lat2)) * Math.Pow(Math.Sin((DiffRadian(lng1, lng2)) / 2.0), 2.0)))));
                 }
             }
 
-            double lat = 0;
-            double lng = 0;
-
-            foreach (var pos in positions)
+            public enum GeoCodeCalcMeasurement : int
             {
-                lat += pos.Latitude;
-                lng += pos.Longitude;
+                Miles = 0,
+                Kilometers = 1
             }
-
-            var total = positions.Count;
-
-            lat = lat / total;
-            lng = lng / total;
-
-            return new Position(lat, lng);
-        }
-        public class DataCalc
-        {
-            public Position Pos { get; set; }
-            public double Distance { get; set; }
-        }
-        // bug ici a test !
-        public static Position[] GetTwoFarestPointsOfCenterPointReference(List<Position> farestPosition, Position centerPosition)
-        {
-            Position[] FarestPos = new Position[2];
-            List<DataCalc> dataCalc = new List<DataCalc>();
-
-            foreach (Position pos in farestPosition)
-            {
-                dataCalc.Add(new DataCalc()
-                {
-                    Pos = pos,
-                    Distance = Math.Sqrt(Math.Pow(pos.Latitude - centerPosition.Latitude, 2) + Math.Pow(pos.Longitude - centerPosition.Longitude, 2))
-                });
-            }
-
-            DataCalc First = new DataCalc() { Distance = 0 };
-            foreach (DataCalc dc in dataCalc)
-            {
-                if (dc.Distance > First.Distance)
-                {
-                    First = dc;
-                }
-            }
-
-            DataCalc Second = new DataCalc() { Distance = 0 };
-            foreach (DataCalc dc in dataCalc)
-            {
-                if (dc.Distance > Second.Distance
-                    && (dc.Pos.Latitude != First.Pos.Latitude && dc.Pos.Longitude != First.Pos.Longitude))
-                {
-                    Second = dc;
-                }
-            }
-
-            FarestPos[0] = First.Pos;
-            FarestPos[1] = Second.Pos;
-
-            return (FarestPos);
-        }
-        public class CameraFocusData
-        {
-            public Position Position { get; set; }
-            public Distance Distance { get; set; }
-        }
-        public static CameraFocusData GetPositionAndZoomLevelForCameraAboutPositions(Position[] FarestPoints)
-        {
-            double earthRadius = 6371000; //metros
-
-            Position pos1 = FarestPoints[0];
-            Position pos2 = FarestPoints[1];
-
-            double latitud1Radianes = pos1.Latitude * (Math.PI / 180.0);
-            double latitud2Radianes = pos2.Latitude * (Math.PI / 180.0);
-            double longitud1Radianes = pos2.Longitude * (Math.PI / 180.0);
-            double longitud2Radianes = pos2.Longitude * (Math.PI / 180.0);
-
-            double deltaLatitud = (pos2.Latitude - pos1.Latitude) * (Math.PI / 180.0);
-            double deltaLongitud = (pos2.Longitude - pos1.Longitude) * (Math.PI / 180.0);
-
-            double sum1 = Math.Sin(deltaLatitud / 2) * Math.Sin(deltaLatitud / 2);
-            double sum2 = Math.Cos(latitud1Radianes) * Math.Cos(latitud2Radianes) * Math.Sin(deltaLongitud / 2) * Math.Sin(deltaLongitud / 2);
-
-            var a = sum1 + sum2;
-            var c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
-
-            var distance = earthRadius * c;
-
-            /* lt is deltaLatitud
-             * lng is deltaLongitud*/
-            var Bx = Math.Cos(latitud2Radianes) * Math.Cos(deltaLongitud);
-            var By = Math.Cos(latitud2Radianes) * Math.Sin(deltaLongitud);
-            var lt = Math.Atan2(Math.Sin(latitud1Radianes) + Math.Sin(latitud2Radianes),
-                                Math.Sqrt((Math.Cos(latitud1Radianes) + Bx) * (Math.Cos(latitud2Radianes) + Bx) + By * By));//Latitud del punto medio
-            var lng = longitud1Radianes + Math.Atan2(By, Math.Cos(longitud1Radianes) + Bx);//Longitud del punto medio
-
-            return (new CameraFocusData() { Position = new Position(lt, lng), Distance = new Distance(distance + 0.2) });
         }
         #endregion
     }
 }
-
-
-/*
- public static Position GetCentralPosition(List<Position> positions)
-        {
-            if (positions.Count == 1)
-            {
-                foreach (Position pos in positions)
-                {
-                    return (pos);
-                }
-            }
-
-            double x = 0;
-            double y = 0;
-            double z = 0;
-
-            foreach (var pos in positions)
-            {
-                var latitude = pos.Latitude * Math.PI / 180;
-                var longitude = pos.Longitude * Math.PI / 180;
-
-                Debug.WriteLine("|Lat = {0}|Long = {1}|", latitude, longitude);
-
-                x += Math.Cos(latitude) * Math.Cos(longitude);
-                y += Math.Cos(latitude) * Math.Sin(longitude);
-                z += Math.Sin(latitude);
-            }
-
-            Debug.WriteLine("Moyennes => |x = {0}|y = {1}|z = {2}|", x, y, z);
-
-            var total = positions.Count;
-
-            Debug.WriteLine("Nombre de positions => |total = {0}|", total);
-
-            x = x / total;
-            y = y / total;
-            z = z / total;
-
-            Debug.WriteLine("Moyennes => |x = {0}|y = {1}|z = {2}|", x, y, z);
-
-            var centralLongitude = Math.Atan2(y, x);
-            var centralSquareRoot = Math.Sqrt(x * x + y * y);
-            var centralLatitude = Math.Atan2(z, centralSquareRoot);
-
-            Debug.WriteLine("Final LAT/LNG => |lat = {0}|lng = {1}|", centralLatitude * 180 / Math.PI, centralLongitude * 180 / Math.PI);
-
-            return new Position(centralLatitude * 180 / Math.PI, centralLongitude * 180 / Math.PI);
-        }
-     */
--- Detailed Part/Controls/Map/MapPinsProject/MapPinsProject/MapPinsProject/Page/MainPage.xaml.cs
+++ Detailed Part/Controls/Map/MapPinsProject/MapPinsProject/MapPinsProject/Page/MainPage.xaml.cs
@@ -17,7 +17,11 @@ namespace MapPinsProject.Page
             CustomPins = new List<CustomPin>()
             {
                 new CustomPin() { Name = "Le Mans", Details = "Famous city for race driver !", Position = new Position(48,0.2) },
-                new CustomPin() { Name = "Ruaudin", Details = "Where I'm coming from.", Position = new Position(47.9450,0.26) }
+                new CustomPin() { Name = "Ruaudin", Details = "Where I'm coming from.", Position = new Position(47.9450,0.26) },
+                new CustomPin() { Name = "Chelles", Details = "Someone there.", Position = new Position(48.877535,2.590160) },
+                new CustomPin() { Name = "Lille", Details = "Le nord..", Position = new Position(50.629250, 3.057256) },
+                new CustomPin() { Name = "Limoges", Details = "I was there ! :o", Position = new Position(45.833619, 1.261105) },
+                new CustomPin() { Name = "Douarnenez", Details = "A trip..", Position = new Position(48.093228,-4.328619) }
             };
 
             InitializeComponent();
